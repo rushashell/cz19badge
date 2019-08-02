7fe3a6ff42c0ac8bb8ceb155c4de281b4593e91d
root <root@sloppy.smurfs.corp>
2019-08-02 22:12:13 +0200

    Added multiplayer to tetris

---
+++ tetris_cz19/menu.py
@@ -0,0 +1,29 @@
+import time
+import uinterface
+
+class TetrisMenu():
+
+  def __init__(self):
+    print("init")
+    self.menu = self._menu_main
+
+  def main(self):
+    self.menu = self._menu_main
+    return self.menu()
+
+  def multiplayer(self):
+    self.menu = self._menu_multiplayer
+    return self.menu()
+
+  def _menu_main(self):
+    items = ["Single player","Multiplayer"]
+    choices = ["singleplayer","multiplayer"]
+    choice = uinterface.menu(items)
+    return choices[choice]
+
+  def _menu_multiplayer(self):
+    items = ["Host","Client"]
+    choices = ["host","client"]
+    choice = uinterface.menu(items)
+    return choices[choice]
+
